2a882339d0b22117466991712ff9f54fa3afec7b
Adrian Guerrero <adrianjguerreros@gmail.com>
2025-10-13 21:21:04 -0300

    Feat: test de caja de búsqueda

--- tests/NewBalance.spec.ts
+++ tests/NewBalance.spec.ts
@@ -1,10 +1,28 @@
 import { test, expect } from '@playwright/test';
 
-    test ('Abrir la página de New Balance Argentina', async ({ page }) => {
+    //abrir la home
+    test('Abrir la página de New Balance Argentina', async ({ page }) => {
   
-        await page.goto ('https://www.newbalance.com.ar/');
+        await page.goto('https://www.newbalance.com.ar/');
         await expect(page).toHaveTitle(/new balance/i);
         await expect(page.locator('header')).toBeVisible();
         await page.waitForTimeout(2000);
    
     });
+
+    // Buscar zapatilla
+    test('Buscar zapatilla cuadro busqueda', async ({ page }) => {
+        await page.goto('https://www.newbalance.com.ar/');
+        await page.locator("//div[@class='search hidden-xs-down']//input[@placeholder='Buscar...']").fill('running');
+        await page.keyboard.press('Enter');
+        await page.waitForLoadState('domcontentloaded');
+        await expect(page.getByText(/Resultados de la búsqueda/i)).toBeVisible();
+        await page.waitForTimeout(2000);
+
+                 
+        
+    });
+
+
+
+    
