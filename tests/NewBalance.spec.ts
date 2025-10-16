b91cc8b40fdf2bfe34b81e1f189e68c5789a297d
Adrian Guerrero <adrianjguerreros@gmail.com>
2025-10-15 22:13:09 -0300

    Feature: Agregar cerrar popup

--- tests/NewBalance.spec.ts
+++ tests/NewBalance.spec.ts
@@ -19,9 +19,42 @@ import { test, expect } from '@playwright/test';
         await expect(page.getByText(/Resultados de la búsqueda/i)).toBeVisible();
         await page.waitForTimeout(2000);
 
-                 
-        
     });
+
+    //Seleccionar zapatilla
+    test('Seleccionar zapatilla', async ({ page }) => {
+        
+        await page.goto('https://www.newbalance.com.ar/');
+        await page.locator("//div[@class='search hidden-xs-down']//input[@placeholder='Buscar...']").fill('running');
+        await page.keyboard.press('Enter');
+        await page.waitForLoadState('domcontentloaded');
+        await expect(page.getByText(/Resultados de la búsqueda/i)).toBeVisible();
+        await page.locator("//img[@id='img-N3R014000']").click(); 
+        await page.waitForTimeout(2000);    
+
+    });
+
+    test('Seleccionar talle y color', async ({ page }) => {
+        
+        await page.goto('https://www.newbalance.com.ar/');
+        await page.waitForTimeout(2000);
+        await page.locator('//a[@href=\'#__cn_close_content\']').click({ timeout: 2000 }).catch(() => {}); //cerrar popup
+        await page.locator("//div[@class='search hidden-xs-down']//input[@placeholder='Buscar...']").fill('running');
+        await page.keyboard.press('Enter');
+        await page.waitForLoadState('domcontentloaded'); // Espera a que cargue la página de resultados
+        await expect(page.getByText(/Resultados de la búsqueda/i)).toBeVisible(); //Espera a que el DOM esté cargado.
+        await page.locator("//img[@id='img-N3R014000']").click(); //talle
+        await page.getByRole('button', { name: 'Seleccionar color red' }).click(); //color
+       
+        await page.pause()
+
+        //await page.waitForTimeout(2000);    
+
+    });
+    
+
+
+    
 
 
 
